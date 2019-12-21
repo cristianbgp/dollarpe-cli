b3f14a0acd695ed7e8bfe67ec57f0931fe403388
cristianbgp <cristian.granda.pastor@gmail.com>
2019-12-20 23:46:04 -0500

    Add support for kambista

--- bin/index.js
+++ bin/index.js
@@ -1,23 +1,38 @@
 #! /usr/bin/env node
 const fetch = require("node-fetch");
 const prettyjson = require("prettyjson");
+const sortCriteriaGenerator = require("./sort-criteria");
 
 async function getRextieData() {
   const response = await fetch("https://app.rextie.com/api/v1/fxrates/rate/", {
     method: "POST"
   });
   const data = await response.json();
-  return data;
+  return { buy: Number(data.fx_rate_buy), sell: Number(data.fx_rate_sell) };
 }
+
+async function getKambistaData() {
+  const response = await fetch(
+    "https://api.kambista.com/v1/exchange/calculates?originCurrency=USD&destinationCurrency=PEN&active=S&amount=1"
+  );
+  const data = await response.json();
+  return { buy: data.tc.bid, sell: data.tc.ask };
+}
+
+const buyCriteriaDesc = sortCriteriaGenerator(item => item[1].buy, {
+  desc: true
+});
 
 async function buildDollarObject() {
   const dollar = {};
-  const rextieData = await getRextieData();
-  dollar.rextie = {
-    buy: rextieData.fx_rate_buy,
-    sell: rextieData.fx_rate_sell
-  };
-  console.log(prettyjson.render(dollar));
+  dollar.rextie = await getRextieData();
+  dollar.kambista = await getKambistaData();
+  const result = Object.fromEntries(
+    Object.entries(dollar).sort(buyCriteriaDesc)
+  );
+  console.log(
+    prettyjson.render(result, { numberColor: "white", keysColor: "cyan" })
+  );
   return prettyjson.render(dollar);
 }
 
