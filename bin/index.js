3e3854bf0f50bfbf1cdbee811a58d75b42b58f4b
cristianbgp <cristian.granda.pastor@gmail.com>
2019-12-22 21:18:33 -0500

    Refactor how to get data

--- bin/index.js
+++ bin/index.js
@@ -3,20 +3,12 @@ const fetch = require("node-fetch");
 const prettyjson = require("prettyjson");
 const sortCriteriaGenerator = require("./sort-criteria");
 
-async function getRextieData() {
-  const response = await fetch("https://app.rextie.com/api/v1/fxrates/rate/", {
-    method: "POST"
+async function getData({ url, method = "GET", accesorToBuy, accesorToSell }) {
+  const response = await fetch(url, {
+    method: method
   });
   const data = await response.json();
-  return { buy: Number(data.fx_rate_buy), sell: Number(data.fx_rate_sell) };
-}
-
-async function getKambistaData() {
-  const response = await fetch(
-    "https://api.kambista.com/v1/exchange/calculates?originCurrency=USD&destinationCurrency=PEN&active=S&amount=1"
-  );
-  const data = await response.json();
-  return { buy: data.tc.bid, sell: data.tc.ask };
+  return { buy: accesorToBuy(data), sell: accesorToSell(data) };
 }
 
 const buyCriteriaDesc = sortCriteriaGenerator(item => item[1].buy, {
@@ -25,8 +17,18 @@ const buyCriteriaDesc = sortCriteriaGenerator(item => item[1].buy, {
 
 async function buildDollarObject() {
   const dollar = {};
-  dollar.rextie = await getRextieData();
-  dollar.kambista = await getKambistaData();
+  dollar.rextie = await getData({
+    url: "https://app.rextie.com/api/v1/fxrates/rate/",
+    method: "POST",
+    accesorToBuy: data => Number(data.fx_rate_buy),
+    accesorToSell: data => Number(data.fx_rate_sell)
+  });
+  dollar.kambista = await getData({
+    url:
+      "https://api.kambista.com/v1/exchange/calculates?originCurrency=USD&destinationCurrency=PEN&active=S&amount=1",
+    accesorToBuy: data => data.tc.bid,
+    accesorToSell: data => data.tc.ask
+  });
   const result = Object.fromEntries(
     Object.entries(dollar).sort(buyCriteriaDesc)
   );
