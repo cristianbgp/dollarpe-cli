a2ff305506b513d2607b67ed1fb9c23544058679
cristianbgp <cristian.granda.pastor@gmail.com>
2020-01-16 15:19:05 -0500

    Fix tkambio data

--- bin/index.js
+++ bin/index.js
@@ -24,15 +24,15 @@ async function buildDollarObject() {
   dollar.tkambio = await getData({
     url: "https://tkambio.com/wp-admin/admin-ajax.php?action=get_tipo_cambio",
     method: "POST",
-    accesorToBuy: data => Number(data.buy_type_change),
-    accesorToSell: data => Number(data.sell_type_change)
+    accesorToBuy: data => Number(data.sell_type_change),
+    accesorToSell: data => Number(data.buy_type_change)
   });
   const result = Object.entries(dollar)
-  .sort(buyCriteriaDesc)
-  .reduce((acc, element) => {
-    acc[element[0]] = element[1];
-    return acc;
-  }, {})
+    .sort(buyCriteriaDesc)
+    .reduce((acc, element) => {
+      acc[element[0]] = element[1];
+      return acc;
+    }, {});
   console.log(
     prettyjson.render(result, { numberColor: "white", keysColor: "cyan" })
   );
